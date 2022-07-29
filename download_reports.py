c8490bac7b3a271a7ea2dd29ba3d0c193985c60d
Victor Castaneda <victor.castaneda@hey.net.co>
2022-07-28 21:29:12 -0500

    Before running.

--- download_reports.py
+++ download_reports.py
@@ -9,7 +9,6 @@ import os
 import paramiko
 import logging
 import boto3
-import time
 
 from botocore.exceptions import ClientError
 from datetime import date
@@ -20,41 +19,23 @@ from io import BytesIO
 load_dotenv()
 
 
-class ManageSFTPFile:
-    def __init__(self, accounts, file_name):
-        self.accounts = accounts
-        self.file_name = file_name
-
-    def download_files(self) -> None:
-        for account in self.accounts:
-            transport = paramiko.Transport(account[2], 22)
-            transport.connect(username=account[4], password=account[3])
-            client = boto3.client(
-                's3',
-                aws_access_key_id=os.getenv("ACCESS_KEY"),
-                aws_secret_access_key=os.getenv("SECRET_KEY"),
-                region_name=os.getenv("REGION")
-            )
-            with paramiko.SFTPClient.from_transport(transport) as sftp:
-                sftp.chdir(path="upload/Report")
-                with BytesIO() as data:
-                    sftp.getfo(f"ACTIVIDADDETALLEDIARIOFTP_20220728.zip", data)
-                    data.seek(0)
-                    try:
-                        response = client.upload_fileobj(
-                            data,
-                            os.getenv("BUCKET_NAME"),
-                            f"{account[2]}_{self.file_name}.zip"
-                        )
-                        return response
-                    except ClientError as error:
-                        logging.error(error)
-                        return None
+def handler(event, context):
+    return {
+        'event': event,
+        'response': Emblue().download_files(),
+        'context': context
+    }
 
 
 class Emblue:
     def __init__(self, searching_date: str = ""):
         self.db_instance = DBInstance(public_key=os.getenv("CLIENT_KEY"))
+        self.client = boto3.client(
+            service_name='s3',
+            region_name=os.getenv("REGION"),
+            aws_access_key_id=os.getenv("ACCESS_KEY"),
+            aws_secret_access_key=os.getenv("SECRET_KEY"),
+        )
         if searching_date:
             self.today = searching_date
         else:
@@ -67,11 +48,34 @@ class Emblue:
     def download_files(self):
         ManageSFTPFile(
             accounts=self.get_emblue_accounts(),
-            file_name=f"{os.getenv('FILE_BASE_NAME')}_{self.today}"
+            file_name=f"{os.getenv('FILE_BASE_NAME')}_{self.today}",
+            client=self.client
         ).download_files()
 
 
-def handler(event, context):
-    return {
-        'response': Emblue().download_files()
-    }
+class ManageSFTPFile:
+    def __init__(self, accounts, file_name, client):
+        self.accounts = accounts
+        self.file_name = file_name
+        self.client = client
+
+    def download_files(self) -> None:
+        for account in self.accounts:
+            transport = paramiko.Transport(account[2], 22)
+            transport.connect(username=account[4], password=account[3])
+
+            with paramiko.SFTPClient.from_transport(transport) as sftp:
+                sftp.chdir(path="upload/Report")
+                with BytesIO() as data:
+                    sftp.getfo(f"{self.file_name}.zip", data)
+                    data.seek(0)
+                    try:
+                        response = self.client.upload_fileobj(
+                            data,
+                            os.getenv("BUCKET_NAME"),
+                            f"{account[2]}_{self.file_name}.zip"
+                        )
+                        return response
+                    except ClientError as error:
+                        logging.error(error)
+                        return None
