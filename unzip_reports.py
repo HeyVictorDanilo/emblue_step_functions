2d4e8b06d519af8e70088fdf2f93f9ebd9f072f5
Victor Castaneda <victor.castaneda@hey.net.co>
2022-08-01 21:06:20 -0500

    Initial point of unzip files.

--- unzip_reports.py
+++ unzip_reports.py
@@ -1,28 +1,44 @@
 try:
-  import unzip_requirements
+    import unzip_requirements
 except ImportError:
-  pass
+    pass
 
-
-import zipfile
+import boto3
+import gzip
 import os
+from io import BytesIO
+from botocore.exceptions import ClientError
+import logging
+import zipfile
 
-
-class ManageZip:
-    def __init__(self, file_name):
-        self.file_name = file_name
-
-    def unzip_local_file(self) -> None:
-        try:
-            with zipfile.ZipFile(
-                f"{self.file_name}.zip", mode="r"
-            ) as archive:
-                archive.extractall()
-        except zipfile.BadZipFile as error:
-            raise error
-        finally:
-            os.remove(f"{self.file_name}.zip")
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def handler(event, context):
-    print("Executing unzip")
+    client = boto3.client(
+        service_name='s3',
+        region_name=os.getenv("REGION"),
+        aws_access_key_id=os.getenv("ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("SECRET_KEY")
+    )
+
+    try:
+        response = client.list_objects(Bucket=os.getenv("BUCKET_NAME"))
+    except ClientError as e:
+        logging.error(e)
+    else:
+        for content in response.get("Contents"):
+            file = BytesIO(client.get_object(Bucket=os.getenv("BUCKET_NAME"), Key=content.get("Key"))['Body'].read())
+            _file = gzip.GzipFile(None, 'rb', fileobj=file)
+            try:
+                client.upload_fileobj(
+                    Fileobj=_file,
+                    Bucket=os.getenv("BUCKET_NAME"),
+                    Key=content.get("Key").replace(".zip", "")
+                )
+            except ClientError as e:
+                logging.error(e)
+            else:
+                logging.info("Uploaded unzipped file")
+    return 1
