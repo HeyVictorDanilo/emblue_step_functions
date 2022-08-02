798af0b15922e7d409ee4187a93cdc471a58d9ec
Victor Castaneda <victor.castaneda@hey.net.co>
2022-08-01 21:46:21 -0500

    Add delete files logic.

--- unzip_reports.py
+++ unzip_reports.py
@@ -27,18 +27,44 @@ def handler(event, context):
     except ClientError as e:
         logging.error(e)
     else:
+        delete_zip_files_data = []
         for content in response.get("Contents"):
-            file = BytesIO(client.get_object(Bucket=os.getenv("BUCKET_NAME"), Key=content.get("Key"))['Body'].read())
-            _file = zipfile.ZipFile(file)
-            for file_name in _file.namelist():
-                try:
-                    client.upload_fileobj(
-                        Fileobj=_file.open(file_name),
-                        Bucket=os.getenv("BUCKET_NAME"),
-                        Key=f'{file_name}'
-                    )
-                except ClientError as e:
-                    logging.error(e)
-                else:
-                    logging.info("Uploaded unzipped file")
+            try:
+                file = BytesIO(client.get_object(Bucket=os.getenv("BUCKET_NAME"), Key=content.get("Key"))['Body'].read())
+            except ClientError as e:
+                logging.error(e)
+            else:
+                delete_zip_files_data.append({"Key": content.get("Key")})
+                _file = zipfile.ZipFile(file)
+                for file_name in _file.namelist():
+                    try:
+                        client.upload_fileobj(
+                            Fileobj=_file.open(file_name),
+                            Bucket=os.getenv("BUCKET_NAME"),
+                            Key=f'{file_name}'
+                        )
+                    except ClientError as e:
+                        logging.error(e)
+                    else:
+                        logging.info("Uploaded unzipped file")
+        delete_zip_files(data=delete_zip_files_data)
+
     return 1
+
+
+def delete_zip_files(data):
+    client = boto3.client(
+        service_name='s3',
+        region_name=os.getenv("REGION"),
+        aws_access_key_id=os.getenv("ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("SECRET_KEY")
+    )
+    try:
+        response = client.delete_objects(
+            Bucket=os.getenv("BUCKET_NAME"),
+            Delete={"Objects": data},
+        )
+    except ClientError as e:
+        logging.error(e)
+    else:
+        return response
