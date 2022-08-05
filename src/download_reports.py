f80bcfa3b0620b35aca50786d4bd0c55b32afceb
Victor Castaneda <victor.castaneda@hey.net.co>
2022-08-05 13:22:32 -0500

    Add date_range logic

--- src/download_reports.py
+++ src/download_reports.py
@@ -26,7 +26,7 @@ def handler(event, context):
 
 
 class Emblue:
-    def __init__(self, searching_date: str = ""):
+    def __init__(self, starting_date: str = "", finishing_date: str = ""):
         self.db_instance = DBInstance(public_key=os.getenv("CLIENT_KEY"))
         self.client = boto3.client(
             service_name='s3',
@@ -34,49 +34,61 @@ class Emblue:
             aws_access_key_id=os.getenv("ACCESS_KEY"),
             aws_secret_access_key=os.getenv("SECRET_KEY"),
         )
-        if searching_date:
-            self.today = searching_date
+        if starting_date:
+            self.starting_date = starting_date
         else:
-            self.today = (date.today() - timedelta(days=1)).strftime("%Y%m%d")
+            self.starting_date = (date.today() - timedelta(days=1)).strftime("%Y%m%d")
 
-    def get_emblue_accounts(self) -> List[Tuple[Any]]:
+        if finishing_date:
+            self.finishing_date = finishing_date
+        else:
+            self.finishing_date = date.today().strftime("%Y%m%d")
+
+    def __get_emblue_accounts(self) -> List[Tuple[Any]]:
         accounts = self.db_instance.handler(query="SELECT * FROM em_blue;")
         return accounts
 
-    def executor(self):
-        manage_sftp_file = ManageSFTPFile(
-            accounts=self.get_emblue_accounts(),
-            file_name=f"{os.getenv('FILE_BASE_NAME')}_{self.today}",
-            client=self.client
+    def __date_range(self):
+        return range(self.starting_date, self.finishing_date)
+
+    def get_files(self):
+        files = ManageSFTPFile.download_files(
+            accounts=self.__get_emblue_accounts(),
+            date_range=self.__date_range()
         )
-        return manage_sftp_file.download_files()
+
 
 
 class ManageSFTPFile:
-    def __init__(self, accounts, file_name, client):
+    def __init__(self, accounts, date_range):
         self.accounts = accounts
-        self.file_name = file_name
-        self.client = client
+        self.date_range = date_range
+
+    @staticmethod
+    def __stablish_conn(account: List[Tuple[Any]]):
+        transport = paramiko.Transport(account[2], 22)
+        transport.connect(username=account[4], password=account[3])
+        with paramiko.SFTPClient.from_transport(transport) as sftp:
+            return sftp
 
     def download_files(self) -> List[str]:
-        sent_files = []
         for account in self.accounts:
-            transport = paramiko.Transport(account[2], 22)
-            transport.connect(username=account[4], password=account[3])
-            with paramiko.SFTPClient.from_transport(transport) as sftp:
-                sftp.chdir(path="upload/Report")
-                with BytesIO() as data:
-                    sftp.getfo(f"{self.file_name}.zip", data)
-                    data.seek(0)
-                    try:
-                        response = self.client.upload_fileobj(
-                            data,
-                            os.getenv("BUCKET_ZIP_FILES"),
-                            f"{account[4]}_{self.file_name}.zip"
-                        )
-                        logging.info(response)
-                        sent_files.append(f"{account[4]}_{self.file_name}.zip")
-                    except ClientError as error:
-                        logging.error(error)
-                        continue
-        return sent_files
+            sftp = self.__stablish_conn(account=account)
+            sftp.chdir(path="upload/Report")
+            for date_file in self.date_range:
+                self.__send_file(sftp_conn=sftp, date_file=date_file)
+
+    def __send_file(self, sftp_conn, date_file):
+        with BytesIO() as data:
+            sftp_conn.getfo(f"{os.getenv('FILE_BASE_NAME')}_{date_file}.zip", data)
+            data.seek(0)
+            try:
+                response = self.client.upload_fileobj(
+                    data,
+                    os.getenv("BUCKET_ZIP_FILES"),
+                    f"_{self.file_name}.zip"
+                )
+            except ClientError as error:
+                logging.error(error)
+            else:
+                return response
