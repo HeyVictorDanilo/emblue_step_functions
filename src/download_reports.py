10f96379c6152796cb751d71320358ea1e00034a
Victor Castaneda <victor.castaneda@hey.net.co>
2022-08-11 14:37:32 -0500

    Running

--- src/download_reports.py
+++ src/download_reports.py
@@ -3,97 +3,50 @@ try:
 except ImportError:
     pass
 
-from src.main_db import DBInstance
-
+from io import BytesIO
 import os
-import paramiko
+import json
 import logging
-import boto3
 
 from botocore.exceptions import ClientError
-from datetime import date, timedelta
 from dotenv import load_dotenv
-from io import BytesIO
+import boto3
+import paramiko
 
 load_dotenv()
 
 
 def handler(event, context):
-    emblue = Emblue()
+    sftp_file = SFTPFile(account=event["account"], date_file=event["file_date"])
     return {
-        'sent_files': emblue.get_files()
+        'response': sftp_file.download_file()
     }
 
 
-class Emblue:
-    def __init__(self, days_difference: int = 5):
-        self.db_instance = DBInstance(public_key=os.getenv("CLIENT_KEY"))
+class SFTPFile:
+    def __init__(self, account, date_file):
+        self.account = account
+        self.date_file = date_file
         self.client = boto3.client(
-            service_name='s3',
+            service_name="s3",
             region_name=os.getenv("REGION"),
             aws_access_key_id=os.getenv("ACCESS_KEY"),
-            aws_secret_access_key=os.getenv("SECRET_KEY"),
+            aws_secret_access_key=os.getenv("SECRET_KEY")
         )
 
-        self.days_difference = days_difference
-
-    def __get_emblue_accounts(self):
-        accounts = self.db_instance.handler(query="SELECT * FROM em_blue;")
-        return accounts
-
-    def get_files(self):
-        sftp_instance = ManageSFTPFile(
-            accounts=self.__get_emblue_accounts(),
-            days_difference=self.days_difference,
-            client=self.client
-        )
-        return sftp_instance.download_files()
-
-
-class ManageSFTPFile:
-    def __init__(self, accounts, days_difference, client):
-        self.accounts = accounts
-        self.days_difference = days_difference
-        self.client = client
-        self.send_files = []
-
-    """
-    @staticmethod
-    def __establish_conn(account):
-        transport = paramiko.Transport(account[2], 22)
-        transport.connect(username=account[4], password=account[3])
-        with paramiko.SFTPClient.from_transport(transport) as sftp:
-            sftp.chdir(path="upload/Report")
-            return sftp
-    """
-
-    def download_files(self):
-        for account in self.accounts:
-            for i in range(0, self.days_difference):
-                try:
-                    response = self.__send_file(
-                        date_file=(date.today() - timedelta(days=i)).strftime("%Y%m%d"),
-                        account=account
-                    )
-                except ClientError as error:
-                    logging.error(error)
-                else:
-                    self.send_files.append(response)
-        return self.send_files
-
-    def __send_file(self, date_file, account):
+    def download_file(self):
         with BytesIO() as data:
-            transport = paramiko.Transport(account[2], 22)
-            transport.connect(username=account[4], password=account[3])
+            transport = paramiko.Transport(self.account[0], 22)
+            transport.connect(username=self.account[1], password=self.account[2])
             with paramiko.SFTPClient.from_transport(transport) as sftp:
                 sftp.chdir(path="upload/Report")
-                sftp.getfo(f"{os.getenv('FILE_BASE_NAME')}_{date_file}.zip", data)
+                sftp.getfo(f"{os.getenv('FILE_BASE_NAME')}_{self.date_file}.zip", data)
                 data.seek(0)
                 try:
                     response = self.client.upload_fileobj(
                         data,
                         os.getenv("BUCKET_ZIP_FILES"),
-                        f"{account[4]}_{os.getenv('FILE_BASE_NAME')}_{date_file}.zip"
+                        f"{self.account[1]}_{os.getenv('FILE_BASE_NAME')}_{self.date_file}.zip"
                     )
                 except ClientError as error:
                     logging.error(error)
